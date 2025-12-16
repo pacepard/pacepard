e0d63858f11f2857d1f6453a4d211320c9747607
Temitope Okuselu <topeokuselu@gmail.com>
2025-12-16 17:25:12 +0100

    chore: registered CORS headers

--- apps/api/src/configs/app.config.ts
+++ apps/api/src/configs/app.config.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction} from "express";
 import cookieParser from "cookie-parser";
 import 'dotenv/config';
 import errorHandler from "../middlewares/error.mdw";
@@ -14,6 +14,13 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
 app.use(cookieParser());
+
+app.use((req: Request, res: Response, next: Function) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 app.use("/api/v1", apiRoutes);
 
